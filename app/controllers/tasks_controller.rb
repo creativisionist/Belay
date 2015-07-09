f936fe3302142660b9d003c007d12e4e0ed09187
Creativisionist <creativisionist@gmail.com>
2015-07-08 19:59:13 -0500

    init task index and creation

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,12 +1,22 @@
 class TasksController < ApplicationController
-before_action :authenticate_parent!, :except => [:index]
-  
+  before_action :authenticate_parent!, :except => [:index]
+
   def index
-    user = User.where(id: params[:user_id])
-    
+    @user = User.where(id: params[:user_id])
+    @tasks = Task.all
   end
 
-  # def new
-  #   @task = Task.new
-  # end
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(to_do: params[:to_do], user_id: params[:user_id], reward_id: params[:reward_id], amount_earned: params[:amount_earned], status: params[:status])
+    if @task.save
+      #flash message
+      redirect_to "/tasks"
+    else
+      render "new"
+    end
+  end
 end
