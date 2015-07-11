0c2631fd8dfe1aed81aeb897a748ea4763b9d661
Creativisionist <creativisionist@gmail.com>
2015-07-11 10:54:26 -0500

    add index, new, create, show methods w/respective views

--- app/controllers/rewards_controller.rb
+++ app/controllers/rewards_controller.rb
@@ -1,2 +1,36 @@
 class RewardsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :authenticate_parent!, :except => [:index]
+
+  def index
+    @user = User.where(id: params[:user_id])
+    @rewards = Reward.all
+  end
+
+  def show
+    reward_id = params[:id]
+    @reward = Reward.find_by(id: reward_id)
+  end
+
+  def new
+    @reward = Reward.new
+  end
+
+  def create
+    @reward = Reward.new(description: params[:description], image_url: params[:image_url], amount_cost: params[:amount_cost], user_id: current_user.id)
+    if @reward.save
+      redirect_to "/rewards"
+    else
+      render "new"
+    end
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
 end
