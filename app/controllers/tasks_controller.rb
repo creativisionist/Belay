c77e85d7558502fc0471790b5319d55bf72d1493
Creativisionist <creativisionist@gmail.com>
2015-07-11 10:11:26 -0500

    add show, edit, update, destroy methods and views

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -7,17 +7,43 @@ class TasksController < ApplicationController
     @tasks = Task.all
   end
 
+  def show
+    task_id = params[:id]
+    @task = Task.find_by(id: task_id)
+  end
+
   def new
     @task = Task.new
   end
 
   def create
     @task = Task.new(to_do: params[:to_do], reward_id: params[:reward_id], amount_earned: params[:amount_earned], status: params[:status], user_id: current_user.id)
+    #add numericality
     if @task.save
       #flash message
       redirect_to "/tasks"
     else
       render "new"
     end
+  end
+
+  def edit
+    task_id = params[:id]
+    @task = Task.find_by(id: task_id)
+  end
+
+  def update
+    task_id = params[:id]
+    @task = Task.find_by(id: task_id)
+    @task.update(to_do: params[:to_do], reward_id: params[:reward_id], amount_earned: params[:amount_earned], status: params[:status], user_id: current_user.id)
+    #flash message
+    redirect_to "/tasks"
+  end
+
+  def destroy
+    task_id = params[:id]
+    task = Task.find_by(id:task_id)
+    task.destroy
+    redirect_to "/tasks"
   end
 end
