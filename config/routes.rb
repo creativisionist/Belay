f88c61d79808377e54e68dd4ff65e1f761de06ba
Creativisionist <creativisionist@gmail.com>
2015-07-14 18:13:39 -0500

    cleaned up routes file

--- config/routes.rb
+++ config/routes.rb
@@ -9,16 +9,4 @@ Rails.application.routes.draw do
   patch '/update_status/:id' => 'tasks#update_status'
   resources :rewards
   patch '/update_reward_status/:id' => 'rewards#update_reward_status'
-  # resources :users do
-  #   resources :tasks
-  #   resources :rewards
-  # end
-
-  # get '/tasks' => 'tasks#index'
-  # get '/tasks/new' => 'tasks#new'
-  # post '/tasks' => 'tasks#create'
-  # get '/tasks/:id' => 'tasks#show'
-  # get '/tasks/:id/edit' => 'tasks#edit'
-  # patch '/tasks/:id' => 'tasks#update'
-  # delete 'tasks/:id' => 'tasks#destroy'
 end
