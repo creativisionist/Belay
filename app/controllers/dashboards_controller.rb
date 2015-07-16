63e12f8042d5622bcee3a7985bdd48baa174fabe
Creativisionist <creativisionist@gmail.com>
2015-07-15 20:02:38 -0500

    make sure parent cannot access child dashboard

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -3,9 +3,13 @@ class DashboardsController < ApplicationController
   before_action :authenticate_parent!, :except => [:child_dashboard]
   
   def child_dashboard
-    @user = User.where(id: params[:user_id])
-    @tasks = current_user.child_tasks.where(status: "incomplete")
-    @current_balance = current_user.total_balance
+    if user_is_child?
+      @user = User.where(id: params[:user_id])
+      @tasks = current_user.child_tasks.where(status: "incomplete")
+      @current_balance = current_user.total_balance
+    else
+      redirect_to "/parent_dashboard"
+    end
   end
 
   def parent_dashboard
