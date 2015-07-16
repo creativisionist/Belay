0fdc90f740229080d6cc6a896dd4a94f7fda712b
Creativisionist <creativisionist@gmail.com>
2015-07-15 19:59:32 -0500

    setup child dashboard

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -3,7 +3,9 @@ class DashboardsController < ApplicationController
   before_action :authenticate_parent!, :except => [:child_dashboard]
   
   def child_dashboard
-
+    @user = User.where(id: params[:user_id])
+    @tasks = current_user.child_tasks.where(status: "incomplete")
+    @current_balance = current_user.total_balance
   end
 
   def parent_dashboard
