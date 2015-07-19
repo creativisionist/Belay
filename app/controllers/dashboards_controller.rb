506b4ed94f099d1620fd25f762e1cd900cc4f184
Creativisionist <creativisionist@gmail.com>
2015-07-18 19:39:40 -0500

    show variables and time for child's investments, respectively: created, duration chosen, days remaining, days passed, rate of interest set, original investment amount, and amount they have the day they check it

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -7,26 +7,32 @@ class DashboardsController < ApplicationController
     if user_is_child?
       @tasks = current_user.child_tasks.where(status: "incomplete")
       @current_balance = current_user.total_balance
-      @current_investments = current_user.child_user_interests.where(withdrawl_status: "accruing")
-
-      # created = Date.parse(investment.created_at.to_s)
-      # now = Date.today
-      # time_passed = now - created
+      @current_investments = current_user.child_user_interests.where.not(withdrawl_status: "paid")
 
       @array_of_each_investment = []
       @total_investments = 0
       @test
       @current_investments.each do |investment|
+        current_i = []
         created = Date.parse(investment.created_at.to_s)
+        current_i.push(created)
         now = Date.today
         time_passed = now - created
+        current_i.push(time_passed.to_i)
         r =  investment.interest_rate.to_f
+        current_i.push(r)
         p =  investment.money_invested.to_f
+        current_i.push(p)
         t =  (time_passed.to_f / 365).to_f
         a = (p * (1 + (r * t))).to_f
-        @test=t
+        current_i.push(a)
+        duration = investment.duration
+        current_i.push(duration)
+        days_remaining = duration.to_i - time_passed.to_i
+        current_i.push(days_remaining)
+        @test=current_i
         @total_investments += a
-        @array_of_each_investment << a
+        @array_of_each_investment.push(current_i)
       end
       p @test
       p @total_investments
